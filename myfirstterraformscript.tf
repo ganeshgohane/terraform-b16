dc9c0c95ee015e293e043003585a30f0a9921cca
ganesh <ganeshsgohane>
2022-05-17 20:35:40 +0530

    --- Auto Git Commit ---

--- myfirstterraformscript.tf
+++ myfirstterraformscript.tf
@@ -6,5 +6,4 @@ resource "aws_instance" "web-server" {
   ami = "ami-079b5e5b3971bd10d"
   instance_type = "t2.micro"
   security_groups = ["default"]
-  key_name = "fstab.pem"
-}
+  key_name = "fstab.pem"
