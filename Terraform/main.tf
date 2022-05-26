2480e738eee931aff113c3c44519002ddfc0e276
ganesh <ganeshsgohane>
2022-05-26 20:21:36 +0530

    --- Auto Git Commit ---

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "web-server" {
-  ami = "ami-079b5e5b3971bd10d"
+  ami = var.ami_id
   instance_type = "t2.micro"
   security_groups = ["default"]
   key_name = "mumbai"
 }
 
 resource "aws_instance" "app-server" {
-  ami = "ami-079b5e5b3971bd10d"
+  ami = var.ami_id
   instance_type = "t2.micro"
   security_groups = ["default"]
   key_name = "mumbai"
