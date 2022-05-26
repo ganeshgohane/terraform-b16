cf2baf3cbc3fac7f2a13367eb7149e5c4a0fa433
ganesh <ganeshsgohane>
2022-05-26 20:42:05 +0530

    --- Auto Git Commit ---

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -19,5 +19,6 @@ variable "ami_id" {
 }
 
 variable "instance_type" {
- description = "please enter instance type t2.micro or t3.micro"
+ description = "please enter instance type t2.micro or t3.micro" 
+ 
 }
