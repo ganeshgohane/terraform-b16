0680b774b424b3021961587dfa053b9c356c8295
ganesh <ganeshsgohane>
2022-05-26 21:34:40 +0530

    --- Auto Git Commit ---

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow_tls" {
   name        = "My_security_group"
   description = "ALLOW ALL TRAFFIC"
-  vpc_id      = aws_vpc
+  vpc_id      = var.vpc
 
   ingress {
     description      = "TLS from VPC"
