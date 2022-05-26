c00f765dedc2d257588061157f22921513a04540
ganesh <ganeshsgohane>
2022-05-26 20:55:07 +0530

    --- Auto Git Commit ---

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,12 +12,3 @@ resource "aws_instance" "app-server" {
   key_name = "mumbai"
 }
  
-variable "ami_id" {
- type = string
- default = "ami-079b5e5b3971bd10d"
- description = "AMI-ID"
-}
-
-variable "instance_type" {
- description = "please enter instance type t2.micro or t3.micro"
-}
