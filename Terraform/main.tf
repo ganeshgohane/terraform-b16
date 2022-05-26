0f85d906ea8ac9f97dc2b04c51f1ef89836bd614
ganesh <ganeshsgohane>
2022-05-26 21:38:43 +0530

    --- Auto Git Commit ---

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,9 +4,9 @@ resource "aws_security_group" "allow_tls" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
+    
+    from_port        = 0
+    to_port          = 0
     protocol         = "tcp"
     cidr_blocks      = [aws_vpc.main.cidr_block]
     ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
