8cb36e49fd8f433e9e86e1140c6659a3a0e5bb20
ganesh <ganeshsgohane>
2022-05-26 21:51:35 +0530

    --- Auto Git Commit ---

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "allow_tls" {
 resource "aws_instance" "app-server" {
   ami = var.ami_id
   instance_type = var.instance_type
-  vpc_security_group_ids =  = [aws_sec]
+  vpc_security_group_ids =  = [aws_security_groups = ]
   key_name = "mumbai"
 }
  
