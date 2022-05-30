d10c4f8d0d0bb3b1ed59a9897c6a529f5c7a0f03
ganesh <ganeshsgohane>
2022-05-30 20:21:17 +0530

    --- Auto Git Commit ---

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -3,3 +3,11 @@ resource "aws_vpc" "vpc-1" {
   region= "ap-south-1"
 }
 
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
