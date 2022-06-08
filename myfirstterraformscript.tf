f7c17bba18b48998181dd5e0071f3ae81fcbcf2a
ganesh <ganeshsgohane>
2022-06-08 19:32:07 +0530

    --- Auto Git Commit ---

--- myfirstterraformscript.tf
+++ myfirstterraformscript.tf
@@ -7,8 +7,8 @@ resource "aws_instance" "web-server" {
   instance_type = "t2.micro"
   security_groups = ["default"]
   key_name = "mumbai"
-  user_data =  = 
-#!/bin/bash
+  user_data = 
+  #!/bin/bash
 yum update
 yum install httpd -y
 echo "<h1> THIS IS HOMEPAGE of "HOSTNAME" </h1>" >/var/www/html/index.html
