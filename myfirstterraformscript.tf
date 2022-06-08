14dbc42fb0fd2ea17022ed570e4f3f00c6e7c05f
ganesh <ganeshsgohane>
2022-06-08 19:32:19 +0530

    --- Auto Git Commit ---

--- myfirstterraformscript.tf
+++ myfirstterraformscript.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "web-server" {
   instance_type = "t2.micro"
   security_groups = ["default"]
   key_name = "mumbai"
-  user_data = 
+  user_data = << EOF
   #!/bin/bash
 yum update
 yum install httpd -y
