ccbe5823f8d156ba5405b31ab15febc568be7492
ganesh <ganeshsgohane>
2022-06-08 19:30:44 +0530

    --- Auto Git Commit ---

---
+++ myfirstterraformscript.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "web-server" {
+  ami = "ami-079b5e5b3971bd10d"
+  instance_type = "t2.micro"
+  security_groups = ["default"]
+  key_name = "mumbai"
+  user_data = << EOF
+#!/bin/bash
+yum update
+yum install httpd -y
+echo "<h1> THIS IS HOMEPAGE of "HOSTNAME" </h1>" >/var/www/html/index.html
+systemctl start httpd
+systemctl enable httpd
+EOF
+}
