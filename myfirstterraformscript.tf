2e7e5a379faa12ad30da32e63fc83169b48f027b
ganesh <ganeshsgohane>
2022-06-08 19:24:51 +0530

    --- Auto Git Commit ---

--- myfirstterraformscript.tf
+++ myfirstterraformscript.tf
@@ -12,8 +12,8 @@ resource "aws_instance" "web-server" {
     yum update -y
     yum install httpd -y
     touch /var/www/html/index.html
-  echo "<h1> hellow world <h1>" >/var/www/html/index.html
-  systemctl start httpd 
+   echo "<h1> hellow world <h1>" >/var/www/html/index.html
+    systemctl start httpd 
   systemctl enable httpd
   EOF 
 }
